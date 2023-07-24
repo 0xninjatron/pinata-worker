38284aabccc030d66344078fb8be6a09e55c89b8
0xninjatron <pulsenft@proton.me>
2023-07-24 12:08:28 -0700

    initial

--- src/worker.js
+++ src/worker.js
@@ -10,6 +10,45 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+		if (request.method === 'POST') {
+			const formData = await request.formData();						
+			const file = formData.get('file'); // 'file' refers to the name attribute in your input element.
+									
+			const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+			
+			const pinataHeaders = {
+			  pinata_api_key: env.PINATA_API_KEY,
+			  pinata_secret_api_key: env.PINATA_SECRET_API_KEY,
+			}
+		
+			const pinataData = new FormData();
+			pinataData.append('file', file);
+		
+			const pinataResponse = await fetch(url, {
+			  method: 'POST',
+			  headers: pinataHeaders,
+			  body: pinataData,
+			})
+		
+			const pinataResult = await pinataResponse.json()
+
+			// console.log("pinataResponse: ", pinataResponse);
+			console.log("pinataResult: ", pinataResult);
+		
+			return new Response(JSON.stringify(pinataResult), {
+				headers: {
+					'content-type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+				}
+			});
+		  } else {
+			return new Response('Please send a POST request.', {
+				headers: {
+					'content-type': 'text/plain',
+					'Access-Control-Allow-Origin': '*',
+				}
+			});
+		  }
+		  }
 };
+
